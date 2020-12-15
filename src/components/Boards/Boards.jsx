f475e5af6b79ddac5d90bbfd761f1f5ef6e0e700
josebayarri <josemiguelbayarri@gmail.com>
2020-12-15 18:34:20 +0100

    board file modificated

--- src/components/Boards/Boards.jsx
+++ src/components/Boards/Boards.jsx
@@ -10,12 +10,27 @@ function Boards() {
       <div className="boards_general">
         <div className="boards_left">
           <div className="caja_tablero">
-            <DashboardOutlinedIcon fontSize="small" />
+            <span className="icon_img">
+              <DashboardOutlinedIcon fontSize="small" />
+            </span>
             <span className="icon_title2">Tableros</span>
           </div>
         </div>
-        <div className="boards_center">soy el centro</div>
-        <div className="boards_right">soy la derecha</div>
+        <div className="boards_center">
+          <h2 className="tablero_title">Tableros personales</h2>
+          <div className="main_tableros">
+            <div className="tableros">
+              <h4 className="tableros_title">Web Development</h4>
+            </div>
+            <div className="tableros">
+              <h4 className="tableros_title">Web Development</h4>
+            </div>
+            <div className="tableros">
+              <h4 className="tableros_title">Web Development</h4>
+            </div>
+          </div>
+        </div>
+        <div className="boards_right"></div>
       </div>
     </div>
   );
