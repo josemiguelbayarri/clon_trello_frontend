c6dcd3177dd8ed84367cc8ee0a8b7ab1631712c9
josebayarri <josemiguelbayarri@gmail.com>
2020-12-16 17:42:46 +0100

    board file modificated

--- src/components/Boards/Boards.jsx
+++ src/components/Boards/Boards.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "../Header/Header";
 import "./Boards.css";
 import DashboardOutlinedIcon from "@material-ui/icons/DashboardOutlined";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
+import AddOutlinedIcon from "@material-ui/icons/AddOutlined";
+import { Modal } from "@material-ui/core";
 
 function Boards() {
   return (
@@ -10,14 +13,49 @@ function Boards() {
       <div className="boards_general">
         <div className="boards_left">
           <div className="caja_tablero">
-            <span className="icon_img">
-              <DashboardOutlinedIcon fontSize="small" />
-            </span>
+            <DashboardOutlinedIcon fontSize="small" />
+
             <span className="icon_title2">Tableros</span>
+          </div>
+          <div className="caja_tablero2">
+            <DashboardOutlinedIcon fontSize="small" />
+
+            <span className="icon_title2">Plantillas</span>
+          </div>
+          <div className="caja_tablero2">
+            <DashboardOutlinedIcon fontSize="small" />
+
+            <span className="icon_title2">Inicio</span>
+          </div>
+          <div className="equipos1">
+            <p>Equipos</p>
+          </div>
+          <div className="equipos2">
+            <AddOutlinedIcon color="action" fontSize="small" />
+
+            <span className="icon_title2">Cree un equipo</span>
           </div>
         </div>
         <div className="boards_center">
-          <h2 className="tablero_title">Tableros personales</h2>
+          <div className="titulo_tableros">
+            <AddOutlinedIcon color="action" />
+            <span className="tablero_title">Creación de Nuevo Tablero</span>
+          </div>
+          <div className="tableros tablero_crear">
+            <h4 className="tableros_title2">Crear un nuevo tablero</h4>
+          </div>
+          {/* <Modal
+            aria-labelledby="modal-title"
+            aria-describedby="modal-description"
+          >
+            <h2 id="modal-title">My Title</h2>
+            <p id="modal-description">My Description</p>
+          </Modal> */}
+          <div className="titulo_tableros">
+            <PersonOutlineOutlinedIcon color="action" />
+            <span className="tablero_title">Tableros personales</span>
+          </div>
+
           <div className="main_tableros">
             <div className="tableros">
               <h4 className="tableros_title">Web Development</h4>
@@ -28,9 +66,17 @@ function Boards() {
             <div className="tableros">
               <h4 className="tableros_title">Web Development</h4>
             </div>
+            <div className="tableros">
+              <h4 className="tableros_title">Web Development</h4>
+            </div>
+            <div className="tableros">
+              <h4 className="tableros_title">Web Development</h4>
+            </div>
+            <div className="tableros">
+              <h4 className="tableros_title">Web Development</h4>
+            </div>
           </div>
         </div>
-        <div className="boards_right"></div>
       </div>
     </div>
   );
