5bbadb27fb2b0ddd4b31d5cc0892f3c567f2a03d
josebayarri <josemiguelbayarri@gmail.com>
2020-12-21 17:35:06 +0100

    modalcenter file modificated

--- src/components/ModalCenter/ModalCenter.jsx
+++ src/components/ModalCenter/ModalCenter.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ModalCenter.css";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
+import DashboardOutlinedIcon from "@material-ui/icons/DashboardOutlined";
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -29,6 +31,8 @@ export default function TransitionsModal() {
     setOpen(false);
   };
 
+  const [color, setColor] = useState("AntiqueWhite");
+
   return (
     <div>
       <div onClick={handleOpen} className="tableros tablero_crear">
@@ -36,9 +40,8 @@ export default function TransitionsModal() {
       </div>
 
       <Modal
-      disableEnforceFocus
-      disableAutoFocus
-    
+        disableEnforceFocus
+        disableAutoFocus
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         className={classes.modal}
@@ -51,11 +54,92 @@ export default function TransitionsModal() {
         }}
       >
         <Fade in={open}>
-          <div className={classes.paper}>
+          {/* <div className={classes.paper}>
             <h2 id="transition-modal-title">Transition modal</h2>
             <p id="transition-modal-description">
               react-transition-group animates me.
             </p>
+          </div> */}
+          <div className="main_modal">
+            <div className="modal_general">
+              <div className="modal_izquierda" style={{ background: color }}>
+                <div className="margenes">
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Añadir título de tablero"
+                  />
+                  <p>Espacio de trabajo de Trello</p>
+                </div>
+              </div>
+              <div className="modal_derecha">
+                <button
+                  className="boton_1"
+                  onClick={() => {
+                    setColor("AntiqueWhite");
+                  }}
+                ></button>
+                <button
+                  className="boton_2"
+                  onClick={() => {
+                    setColor("DarkOrchid");
+                  }}
+                ></button>
+                <button
+                  className="boton_3"
+                  onClick={() => {
+                    setColor("orange");
+                  }}
+                ></button>
+                <button
+                  className="boton_4"
+                  onClick={() => {
+                    setColor("YellowGreen");
+                  }}
+                ></button>
+                <button
+                  className="boton_5"
+                  onClick={() => {
+                    setColor("purple");
+                  }}
+                ></button>
+                <button
+                  className="boton_6"
+                  onClick={() => {
+                    setColor("pink");
+                  }}
+                ></button>
+                <button
+                  className="boton_7"
+                  onClick={() => {
+                    setColor("yellow");
+                  }}
+                ></button>
+                <button
+                  className="boton_8"
+                  onClick={() => {
+                    setColor("green");
+                  }}
+                ></button>
+                <button
+                  className="boton_9"
+                  onClick={() => {
+                    setColor("red");
+                  }}
+                ></button>
+              </div>
+            </div>
+            <div className="submodal">
+              <div className="modal_abajo">
+                <button>Crear tablero</button>
+                <DashboardOutlinedIcon fontSize="small" />{" "}
+                <span className="plantilla_crear">
+                  {" "}
+                  Empiece con una plantilla
+                </span>
+              </div>
+            </div>
           </div>
         </Fade>
       </Modal>
